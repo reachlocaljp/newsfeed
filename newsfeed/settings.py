d45f16e9f6591ace5f8f238dba80669a6a132a31
Haroun Chin <chin.haroun@gmail.com>
2019-02-16 02:59:17 +0900

    reorganized project folder

--- newsfeed/settings.py
+++ newsfeed/settings.py
@@ -1,3 +1,4 @@
+from .secret_key import SECRET_KEY
 """
 Django settings for newsfeed project.
 
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
