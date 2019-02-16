df668f7bc3bcfc484bf4a07f41c78507d135998a
Haroun Chin <chin.haroun@gmail.com>
2019-02-16 15:02:26 +0900

    wrote the view to display the latest 20 articles

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-from .views import ArticleView
+from .views import ArticleAPIView, ArticleView
 
 
 urlpatterns = [
-    path('api/articles', ArticleView.as_view()),
+    path('api/articles', ArticleAPIView.as_view()),
+    path('articles', ArticleView.as_view())
 ]
--- apps/news/views.py
+++ apps/news/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import TemplateView
 
 from .models import Article
 
 
-class ArticleView(View):
+class ArticleAPIView(View):
     def get(self, request):
         articles = Article.objects.all().order_by('-published_at')[:100]
         return HttpResponse('Hello World')
+
+
+class ArticleView(TemplateView):
+    template_name = "news/news.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticleView, self).get_context_data(*args, **kwargs)
+        queryset = Article.objects.all().order_by('-published_at')[:20]
+        context['article_list'] = queryset
+        return context
